8c8f88a40af74a376b7c2ffbe64b4d2c52d529a2
Monish <monish.mukherjee1994@gmail.com>
2019-04-11 00:24:32 -0700

    sorting the phases alphabetically - ABCN

--- blazegraph/src/main/java/gov/pnnl/goss/cim2glm/CIMImporter.java
+++ blazegraph/src/main/java/gov/pnnl/goss/cim2glm/CIMImporter.java
@@ -11,6 +11,7 @@ import java.util.HashMap;
 import java.util.SortedSet;
 import java.util.TreeSet;
 import java.util.List;
+import java.util.Arrays;
 
 import org.apache.jena.query.*;
 
@@ -780,6 +781,8 @@ public class CIMImporter extends Object {
 				break;
 			}
 		}
+		//sorting the wire phase array alphabetically (Edits by MuMonish)
+		Arrays.sort(ln.wire_phases);
 		for (int i = 0; i < ln.nwires; i++) {
 			if (ln.wire_phases[i].equals ("A")) {
 				match_A = GldLineConfig.GetMatchWire (ln.wire_classes[i], ln.wire_names[i]);
